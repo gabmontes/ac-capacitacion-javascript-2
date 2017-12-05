fa54b89384e23d479b0c9befe2bd1315f7099856
Gabriel Montes <gabriel@utoctadel.com.ar>
2017-12-05 16:34:16 -0300

    Agrega ejemplo de módulos CommonJS en Node.JS

---
+++ clase9/modulos/index.js
@@ -0,0 +1,4 @@
+const { rnd, PI } = require('./lib')
+
+console.log(rnd())
+console.log(PI())
---
+++ clase9/modulos/lib.js
@@ -0,0 +1,9 @@
+function rnd() {
+  return Math.random()
+}
+
+function PI() {
+  return Math.PI
+}
+
+module.exports = { rnd, PI }
